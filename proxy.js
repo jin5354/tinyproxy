e623a351171a50ad067a9cfcebccbefb7992a666
jin5354 <xiaoyanjinx@gmail.com>
2016-02-13 18:26:18 +0800

    update
    full http & https support

--- proxy.js
+++ proxy.js
@@ -1,71 +1,119 @@
 'use strict';
 const http = require('http');
 const url = require('url');
-//const request = require('request');
+const net = require('net');
 
 let port;
 
-function Proxy() {
-    this.port = 12222;
-    this.onServerError = function() {};
-    this.onBeforeRequest = function() {
-        //_webContents.send('HTTPData', 'req', req);
-    };
-    this.onBeforeResponse = function() {
-        //_webContents.send('HTTPData', 'res', res);
-    };
-    this.onRequestError = function() {};
-}
+class Proxy {
 
-Proxy.prototype.requestHandler = (req, res) => {
-
-    try {
-        
-        var self = this; // this -> server
-        var path = req.headers.path || url.parse(req.url).path;
-        var requestOptions = {
-            host: req.headers.host.split(':')[0],
-            port: req.headers.host.split(':')[1] || 80,
-            path: path,
-            method: req.method,
-            headers: req.headers
-        };
-
-        console.log(requestOptions.host+requestOptions.path);
-
-        //若访问的是本proxy，给一个提示
-        if (requestOptions.host == '127.0.0.1' && requestOptions.port == port) {
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.write('proxy works ok');
-            res.end();
-            return;
-        }
-
-        sendRequest(requestOptions, req, res);
-
-    } catch (e) {
-        console.log('requestHandlerError' + e.message);
+    constructor(options) {
+        this.port = options.port;
+        this.onServerError = options.onServerError || () => {};
+        this.onBeforeRequest = options.onBeforeRequest || () => {};
+        this.onBeforeResponse = options.onBeforeResponse || () => {};
+        this.onRequestError = options.onRequestError || () => {};
     }
 
-};
+    requestHandler(req, res) {
 
-Proxy.prototype.start = function() {
+        try {
+            
+            let path = req.headers.path || url.parse(req.url).path;
+            let requestOptions = {
+                host: req.headers.host.split(':')[0],
+                port: req.headers.host.split(':')[1] || 80,
+                path: path,
+                method: req.method,
+                headers: req.headers
+            };
 
-    let server = http.createServer();
+            console.log(requestOptions.host+requestOptions.path);
 
-    server.on('request', this.requestHandler);
-    //server.on('connect', this.connectHandler);
+            //若访问的是本proxy，给一个提示
+            if (requestOptions.host == '127.0.0.1' && requestOptions.port == port) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('proxy works ok');
+                res.end();
+                return;
+            }
 
-    server.on('error', this.onServerError);
-    server.on('beforeRequest', this.onBeforeRequest);
-    server.on('beforeResponse', this.onBeforeResponse);
-    server.on('requestError', this.onRequestError);
+            sendRequest(requestOptions, req, res);
 
-    server.listen(this.port);
-    port = this.port;
-};
+        } catch (e) {
+            //console.log('requestHandlerError' + e.message);
+        }
+
+    }
+
+    connectHandler(req, socket, head) {
+        try {
+
+            let requestOptions = {
+                host: req.url.split(':')[0],
+                port: req.url.split(':')[1] || 443
+            };
+
+            let ontargeterror = () => {
+
+                _synReply(socket, 502, 'Tunnel Error', {}, function() {
+                    try {
+                        socket.end();
+                    }
+                    catch(e) {
+                        console.log(`end error: ${e.message}`);
+                    }
+
+                });
+            };
+
+            let connectRemote = (requestOptions, socket) => {
+                let tunnel = net.createConnection(requestOptions, function() {
+                    //format http protocol
+                    _synReply(socket, 200, 'Connection established', {
+                        'Connection': 'keep-alive'
+                    }, function(e) {
+                        if (e) {
+                            console.log(`syn error: ${e.message}`);
+                            tunnel.end();
+                            socket.end();
+                            return;
+                        }
+                        tunnel.pipe(socket);
+                        socket.pipe(tunnel);
+                    });
+                });
+
+                tunnel.setNoDelay(true);
+                tunnel.on('error', ontargeterror);
+            };
+
+            this.emit('beforeRequest', requestOptions);
+            connectRemote(requestOptions, socket);
+        } catch (e) {
+            console.log(`connectHandler error: ${e.message}`);
+        }
+
+    }
+
+    start() {
+
+        let server = http.createServer();
+
+        server.on('request', this.requestHandler);
+        server.on('connect', this.connectHandler);
+
+        server.on('error', this.onServerError);
+        server.on('beforeRequest', this.onBeforeRequest);
+        server.on('beforeResponse', this.onBeforeResponse);
+        server.on('requestError', this.onRequestError);
+
+        server.listen(this.port);
+        port = this.port;
+    }
+}
 
 let sendRequest = (requestOptions, req, res) => {
     let rReq = http.request(requestOptions, function(rRes) {
@@ -74,17 +122,13 @@ let sendRequest = (requestOptions, req, res) => {
         res.writeHead(rRes.statusCode, '', rRes.headers);
 
         rRes.pipe(res);
-        // Res could not write, but it could close connection
         res.pipe(rRes);
 
     });
 
-    rReq.on('error', function(e) {
-        console.log('requestError', e, req, res);
+    rReq.on('error', function() {
 
         res.writeHead(502, 'Proxy fetch failed');
-//            res.end();
-//            remoteRequest.end();
     });
 
     req.pipe(rReq);
@@ -94,5 +138,17 @@ let sendRequest = (requestOptions, req, res) => {
     });
 };
 
-let tinyProxy = new Proxy();
-tinyProxy.start();
+let _synReply = (socket, code, reason, headers, cb) => {
+    try {
+        let statusLine = 'HTTP/1.1 ' + code + ' ' + reason + '\r\n';
+        let headerLines = '';
+        for (var key in headers) {
+            headerLines += key + ': ' + headers[key] + '\r\n';
+        }
+        socket.write(statusLine + headerLines + '\r\n', 'UTF-8', cb);
+    } catch (error) {
+        cb(error);
+    }
+};
+
+module.exports = Proxy;
