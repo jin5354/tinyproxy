44917203dda312770ef7e4c76df4f214168a2124
jin5354 <xiaoyanjinx@gmail.com>
2016-02-15 21:05:13 +0800

    update
    update

--- proxy.js
+++ proxy.js
@@ -2,8 +2,28 @@
 const http = require('http');
 const url = require('url');
 const net = require('net');
+const tls = require('tls');
+const request = require('request');
+const socksClient = require('socks5-client');
 
-let port;
+let _port;
+
+class Agent extends http.Agent {
+
+    constructor(options, protocol) {
+        super(options);
+        this.socksHost = options.socksHost || 'localhost';
+        this.socksPort = options.socksPort || 1080;
+        if(protocol === 'http') {
+            this.createConnection = socksClient.createConnection;
+        }else if(protocol === 'https') {
+            this.defaultPort = 443;
+            this.protocol = 'https:';
+            this.createConnection = createConnection;
+        }
+    }
+
+}
 
 class Proxy {
 
@@ -18,8 +38,9 @@ class Proxy {
     requestHandler(req, res) {
 
         try {
-            
+            delete req.headers['proxy-connection'];
             let path = req.headers.path || url.parse(req.url).path;
+
             let requestOptions = {
                 host: req.headers.host.split(':')[0],
                 port: req.headers.host.split(':')[1] || 80,
@@ -28,10 +49,10 @@ class Proxy {
                 headers: req.headers
             };
 
-            console.log(requestOptions.host+requestOptions.path);
+            console.log(`requestHandler: http: ${req.headers.host}`);
 
             //若访问的是本proxy，给一个提示
-            if (requestOptions.host == '127.0.0.1' && requestOptions.port == port) {
+            if (requestOptions.host == '127.0.0.1' && requestOptions.port == _port) {
                 res.writeHead(200, {
                     'Content-Type': 'text/plain'
                 });
@@ -40,10 +61,13 @@ class Proxy {
                 return;
             }
 
+            let agent = new Agent(requestOptions, 'http');
+            requestOptions.agent = agent;
+
             sendRequest(requestOptions, req, res);
 
         } catch (e) {
-            //console.log('requestHandlerError' + e.message);
+            console.log(`requestHandlerError': ${e.message}`);
         }
 
     }
@@ -55,6 +79,8 @@ class Proxy {
                 host: req.url.split(':')[0],
                 port: req.url.split(':')[1] || 443
             };
+
+            console.log(`connectHandler: https: ${req.headers.host}`);
 
             let ontargeterror = () => {
 
@@ -68,6 +94,9 @@ class Proxy {
 
                 });
             };
+
+            let agent = new Agent(requestOptions, 'https');
+            requestOptions.agent = agent;
 
             let connectRemote = (requestOptions, socket) => {
                 let tunnel = net.createConnection(requestOptions, function() {
@@ -111,11 +140,12 @@ class Proxy {
         server.on('requestError', this.onRequestError);
 
         server.listen(this.port);
-        port = this.port;
+        _port = this.port;
     }
 }
 
 let sendRequest = (requestOptions, req, res) => {
+
     let rReq = http.request(requestOptions, function(rRes) {
 
         // write out headers to handle redirects
@@ -149,6 +179,32 @@ let _synReply = (socket, code, reason, headers, cb) => {
     } catch (error) {
         cb(error);
     }
+};
+
+let createConnection =(options) => {
+
+    let socksSocket, handleSocksConnectToHost;
+
+    socksSocket = socksClient.createConnection(options);
+
+    handleSocksConnectToHost = socksSocket.handleSocksConnectToHost;
+    socksSocket.handleSocksConnectToHost = function() {
+        options.socket = socksSocket.socket;
+        options.servername = options.hostname;
+
+        socksSocket.socket = tls.connect(options, function() {
+
+            // Set the 'authorized flag for clients that check it.
+            socksSocket.authorized = socksSocket.socket.authorized;
+            handleSocksConnectToHost.call(socksSocket);
+        });
+
+        socksSocket.socket.on('error', function(err) {
+            socksSocket.emit('error', err);
+        });
+    };
+
+    return socksSocket;
 };
 
 module.exports = Proxy;
